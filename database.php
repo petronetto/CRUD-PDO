cdbcf603d44c2ae911685817bf3db589ea01750f
Petronetto <juliano@petronetto.com.br>
2015-09-21 22:40:23 -0300

    Adjusments

--- database.php
+++ database.php
@@ -1,6 +1,5 @@
 <?php
-class Database
-{
+class Database {
     private static $dbName = 'pdo' ;
     private static $dbHost = 'localhost' ;
     private static $dbUsername = 'root';
@@ -12,25 +11,20 @@ class Database
         die('Init function is not allowed');
     }
      
-    public static function connect()
-    {
+    public static function connect() {
        // One connection through whole application
-       if ( null == self::$cont )
-       {     
-        try
-        {
+       if ( null == self::$cont ) {     
+        try {
           self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
         }
-        catch(PDOException $e)
-        {
+        catch(PDOException $e) {
           die($e->getMessage()); 
         }
        }
        return self::$cont;
     }
      
-    public static function disconnect()
-    {
+    public static function disconnect() {
         self::$cont = null;
     }
 }
